02a2c935d391159ef7f56737f7ec671bd634c60b
mert <mertyazi44@hotmail.com>
2024-03-08 15:28:59 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ForegroundService
+
+It's an example to show how to use foreground services.<br /><br />
+Project is written in Kotlin.💯
---
+++ app/src/main/java/com/example/foregroundservice/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.foregroundservice
+
+import android.Manifest
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.foregroundservice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                0
+            )
+        }
+
+        binding.button.setOnClickListener {
+            Intent(applicationContext, RunningService::class.java).also {
+                it.action = RunningService.Actions.START.toString()
+                startService(it)
+            }
+        }
+
+        binding.button2.setOnClickListener {
+            Intent(applicationContext, RunningService::class.java).also {
+                it.action = RunningService.Actions.STOP.toString()
+                startService(it)
+            }
+        }
+    }
+}
